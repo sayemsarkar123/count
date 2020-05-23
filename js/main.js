21df50b57ba388f9442bd5afb280df0dfe7d9e5b
Abu Sayem Sarkar <sayemsarkar123@gmail.com>
2020-05-23 16:19:13 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,5 +7,11 @@ $(function () {
     $(this).html(event.strftime('<div><span>%D</span>Days</div><div><span>%H</span>Hours</div><div><span>%M</span>Minutes</div><div><span>%S</span>Seconds</div>'));
   });
   
+  $(".main").ripples({
+  resolution: 512,
+  dropRadius: 10,
+  perturbance: 0.5
+  });
+  
 
 })(jQuery);
